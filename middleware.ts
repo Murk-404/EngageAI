b6d5b599d203139c3e5c616f9593f116e9f383a9
Tyler Fullmer <fullmtyl@gmail.com>
2023-05-12 18:23:31 -0500

    Fixing build issue with middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
-console.log("middleware.ts");
+
 export default authMiddleware();
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/'"],
 };
